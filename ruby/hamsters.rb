1f4b0c657043a0941b76420bae80304ad71f4cf0
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-11 19:47:11 -0700

    Finished 4.3 Release 2 and 3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,16 @@
 puts "What's the hamsters name?"
 name = gets.chomp
 
+volume = nil
+candidate = nil
+age = nil
+
+
 loop do
 puts "What's the volume level of the hamster (1 to 10)?"
 volume = gets.chomp.to_i
 	if volume <= 10 && volume >= 1
+		volume = volume.to_s
 		break
 	end
 end
@@ -16,7 +22,12 @@ loop do
 puts "Is the hamster a good candidate for adoption?"
 candidate = gets.chomp.downcase
 	if candidate == "yes" || candidate == "no" || candidate == "y" || candidate == "n"
-		break
+		if candidate == "y"
+			candidate = "yes"
+		elsif candidate == "n"
+			candidate = "no"
+		end	
+	break
 	else
 		puts "Try again"
 	end
@@ -25,10 +36,27 @@ end
 loop do
 puts "What's the hamsters estimated age?"
 age = gets.chomp
-	if age.to_i >= 0
+	if age.to_i > 0
+		break
+	elsif age == "0"
+		age = age.to_i
 		break
 	elsif age == ""
 		age = nil
 		break
+	else
+		puts "Invalid response. Try again."
 	end
+end
+
+
+puts "Hamster's name is: #{name}."
+puts "Hamster's volume level is: #{volume}."
+puts "Hamster's fur color is #{fur_color}"
+puts "Is the hamster recommended for adoption: #{candidate}"
+
+if age != nil
+	puts "Hamster's age is #{age}"
+else 
+	puts "Hamster's age is unknown."
 end
