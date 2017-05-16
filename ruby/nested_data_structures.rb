ec1dc83026d47e220612ab30488c38c0d9c52d21
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-16 14:05:11 -0700

    First draft of school complete

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,31 @@
+school = {
+	rooms: {
+		classroom: {
+			student_desk: ['paper', 'pens', 'pencils'],
+			teacher_desk: ['gradebook', 'markers', 'guidebook']
+			},
+		gym: {
+			locker_room: {
+				lockers: [100, 101, 102, 103],
+				towels: [1, 2, 3, 4, 5]
+				},
+			basketball_court: {
+				hoops: [1,2],
+				balls: [1, 2, 3, 4]
+				},
+			},
+		cafeteria: {
+			food: ['oranges', 'carrots', 'chicken'],
+			supplies: [['tables', 'chairs', 'food stands'], ['forks', 'knives', 'spoons', 'napkins'], ['plates', 'bowls', 'trays']] 
+			}
+	},
+
+	faculty: {
+		principal: "George Smith",
+		vice_principal: "Frank Jones",
+		teachers: ['Mr. Doe', 'Mrs. Doe', 'Mr. Adams'],
+		janitors: ['Mark G.', 'Kevin A.', 'Bob L.']
+	},
+
+	students: [['Anthony A.', 'Grade 10', 'A average'], ['Brooke B.', 'Grade 9', 'B average'], ['Calvin C.', 'Grade 10', 'C average'], ['Derek D.', 'Grade 11', 'A average'], ['Erik E.', 'Grade 12', 'B average']]
+}
