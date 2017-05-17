9a616bb09f355319d45a756001a97640bd2ab67e
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-16 19:18:37 -0700

    Finish Release 1 for 5.5, cleaned up encryption method code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -23,7 +23,6 @@ def encrypt (full_name)
 	first_name = full_name.split(' ')[0]
 	last_name = full_name.split(' ')[1]
 	first_name = first_name.split('')
-	last_name = last_name.split('')
 
 	first_name.map! do |char|
 		if is_vowel?(char)
@@ -32,23 +31,29 @@ def encrypt (full_name)
 			char = char
 		end
 	end
-
-	last_name.map! do |char|
-		if is_vowel?(char)
-			next_vowel(char)
-		else
-			char = char
-		end
-	end
-
 	first_name_encrypt = first_name.join('')
-	last_name_encrypt = last_name.join('')
-	full_name_encrypt = last_name_encrypt + " " + first_name_encrypt
-	puts full_name_encrypt
+
+	if last_name != nil
+		last_name = last_name.split('')
+		last_name.map! do |char|
+			if is_vowel?(char)
+				next_vowel(char)
+			else
+				char = char
+			end
+		end
+		last_name_encrypt = last_name.join('')
+		full_name_encrypt = last_name_encrypt + " " + first_name_encrypt
+	else
+		full_name_encrypt = first_name_encrypt
+	end	
 end
 
-name1 = "Vussit Gimodoe"
-encrypt(name1)
-
-name2 = "Felicia Torres" 
-encrypt(name2)
+loop do 
+	puts "Please enter your name (type 'quit' to exit):"
+	name = gets.chomp
+	if name == "quit"
+		break
+	end
+	puts encrypt(name)
+end
