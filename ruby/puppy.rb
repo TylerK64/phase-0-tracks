2f247a7f5af383728feba28987e727d2dbd6ed88
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-22 13:27:32 -0700

    Add psuedocode for puppy class in puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+=begin
+SPECIES ---------------------
+Mini Doggo aka Pupper
+
+CHARACTERISTICS -------------------------------
+Friendliness: high
+Cuteness: very high
+Name: varies
+Color: varies
+Ears: big
+
+BEHAVIOR ------------------------------------
+Tail wag
+Lick
+Sniff
+Eat
+=end
