c8b8f506b6351e9b9ec77c7c477318a14923607f
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-22 14:35:19 -0700

    Add code for 5.3 Release 1, gave initial attribute values for santa array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,15 @@
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
+    #puts "Also my ethnicity is #{@ethnicity}"
   end
 
   def eat_milk_and_cookies(cookie_type)
@@ -12,6 +17,15 @@ class Santa
   end
 end
 
-santa1 = Santa.new
-santa1.speak
-santa1.eat_milk_and_cookies("chocolate chip")
+#santa1 = Santa.new
+#santa1.speak
+#santa1.eat_milk_and_cookies("chocolate chip")
+
+santas = []
+genders = ["male", "female", "other", "gender fluid", "unsure", "male", "female"]
+ethnicities = ["white", "black", "Chinese", "Italian", "Russian", "Canadian", "Native American"]
+
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+  santas[i].speak
+end
