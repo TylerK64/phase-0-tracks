d99674bc8ca7a1cda286d11b8f98587f793c175a
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-22 15:35:29 -0700

    Add code for 6.3 Release 4 - Made 1000 sample of santas with random attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-  attr_reader :age, :ethnicity, :reindeer_ranking
-  attr_accessor :gender
+  attr_reader :ethnicity, :reindeer_ranking
+  attr_accessor :gender, :age
 
   def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
@@ -38,9 +38,10 @@ end
 #santa1.eat_milk_and_cookies("chocolate chip")
 
 santas = []
-genders = ["male", "female", "other", "gender fluid", "unsure", "male", "female"]
-ethnicities = ["white", "black", "Chinese", "Italian", "Russian", "Canadian", "Native American"]
+genders = ["male", "female", "other", "gender fluid", "unsure", "agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["white", "black", "Chinese", "Italian", "Russian", "Canadian", "Native American", "Latino", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
+=begin
 genders.length.times do |i|
   santas << Santa.new(genders[i], ethnicities[i])
   santas[i].celebrate_birthday
@@ -50,3 +51,13 @@ end
 puts santas[0].gender=("not male")
 santas[0].get_mad_at("Vixen")
 puts santas[0].reindeer_ranking
+=end
+
+n = 0
+until n == 1000
+  santas << Santa.new(genders.sample, ethnicities.sample)
+  santas[n].age = rand(0..140)
+  puts santas[n].gender, santas[n].age, santas[n].ethnicity
+  p santas[n].reindeer_ranking
+  n += 1
+end
