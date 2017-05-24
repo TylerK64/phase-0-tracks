c8897cd28bf825ca36e6fc53f1d367d9c78bc636
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-24 15:23:03 -0700

    Add two methods: one to check guess, other to check win condition

--- ruby/Game_Class/game.rb
+++ ruby/Game_Class/game.rb
@@ -1,32 +1,45 @@
 #psuedocode
 # write class Game
 #attributes: correct answer, guess answers (array), guess counter, is_over (boolean), guess output, win (true)/lose (false)
-#methods: check guess, check repeat answer, update output, check game status for win or lose condition
+#methods: check guess, check repeat answer, update output/feedback, check game status for win or lose condition
 #TDD: write basic game class stuct, check input & output; check repeatable word; check correct output
 
 class Game
-  attr_reader :answer, :guess_feedback, :game_status, :guess_answers
-  attr_accessor :guess_count
+  attr_reader :game_status, :guess_count
 
   def initialize(str)
     @answer = str
     @guess_answers = []
     @guess_count = @answer.length
-    @is_over = false
     @guess_feedback
-    @game_status = false
+    @game_status
   end
 
   def check_guess(str)
-    @guess_answers << str
+    if !@guess_answers.include?(str) #if guess_answers doesn't include str, add str to the array
+      @guess_answers << str
+      @guess_count -= 1
+    end
     p @guess_answers
   end
 
-  def check_repeat(str)
+  def feedback
+
+  end
+
+  def check_status
     @guess_answers.each do |i|
-      if str == i
-        @guess_answers.delete_at(@guess_answers.index(i))
+      if i == @answer
+        puts "You win!"
+        @game_status = true
+      else
+        false         
       end
+    end
+
+    if @guess_count == 0
+      puts "Sorry, you lose"
+      @game_status = false
     end
   end
 
@@ -37,10 +50,9 @@ answer = gets.chomp.strip
 
 game = Game.new(answer)
 
-until game.guess_count == 0 || game.game_status != false
+until game.guess_count == 0 || game.game_status == true
   puts "Please enter your guess word:"
   guess = gets.chomp.strip
   game.check_guess(guess)
-  #game.check_repeat(guess)
-  game.guess_count -= 1
+  game.check_status
 end
