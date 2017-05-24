ade80f8df61f8ca0dcae12fd9d26ec98a3412a1b
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-24 13:46:06 -0700

    Add basic structure for Game class & game_spec.rb

--- ruby/Game_Class/game.rb
+++ ruby/Game_Class/game.rb
@@ -1,9 +1,24 @@
 #psuedocode
 # write class Game
-#attributes: correct answer, guess answers (array), guess counter, is_over (boolean), guess output, win/lose
-#methods: check guess, update output, check repeate answer
-#TDD: write basic game class stuct, input & output; check repeatable word; check correct output
+#attributes: correct answer, guess answers (array), guess counter, is_over (boolean), guess output, win (true)/lose (false)
+#methods: check guess, update output, check repeate answer, check if win or lose
+#TDD: write basic game class stuct, check input & output; check repeatable word; check correct output
 
 class Game
-  
+  attr_reader :answer, :guess_feedback
+  attr_accessor :guess_count
+
+  def initialize(str)
+    @answer = str
+    @guess_answers = []
+    @guess_count = 0
+    @is_over = false
+    @guess_feedback
+    @game_status
+  end
 end
+
+puts "Please enter your secret word:"
+answer = gets.chomp.strip
+
+game = Game.new(answer)
--- ruby/Game_Class/game_spec.rb
+++ ruby/Game_Class/game_spec.rb
@@ -1,1 +1,8 @@
-#text, need code
+require_relative 'game'
+
+describe Game do
+  let(:game) { Game.new }
+
+  it "first test" do
+  end
+end
