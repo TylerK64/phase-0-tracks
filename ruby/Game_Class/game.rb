c1d913c1b6ddf5a0c99db157c8c0f14e802d4c66
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-24 13:22:15 -0700

    Add psuedocode for Game class and TDD

--- ruby/Game_Class/game.rb
+++ ruby/Game_Class/game.rb
@@ -1,2 +1,9 @@
+#psuedocode
+# write class Game
+#attributes: correct answer, guess answers (array), guess counter, is_over (boolean), guess output, win/lose
+#methods: check guess, update output, check repeate answer
+#TDD: write basic game class stuct, input & output; check repeatable word; check correct output
+
 class Game
+  
 end
