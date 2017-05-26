bca71623173bfe35ff6f116a091548064fe0131e
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-25 17:51:12 -0700

    Add psueocode to gps 2.2

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,10 +1,9 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-  # [fill in any steps here]
-  # set default quantity
+  # Initialize with blank array, split parameter and iterate through the list with default quantity = 1
   # print the list to the console [can you use one of your other methods here?]
-# output: [what data type goes here, array or hash?]
+# output: hash
 def create_list(string)
   list = {}
   string.split(' ').each {|item| list[item] = 1}
@@ -12,24 +11,24 @@ def create_list(string)
 end
 # Method to add an item to a list
 # input: list, item name, and optional quantity
-# steps:
-# output:
+# steps: choose list to add item, name item, and choose whether we want to add to the default quantity
+# output: hash
 def add_item(list,item,quantity=1)
   list[item] = quantity
   list
 end
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: list, item name
+# steps: choose list to add item, name item that is to be removed
+# output: hash
 def remove_item(list,item)
   list.delete_if {|key,value| key == item}
   list
 end
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: list, item name, quanitity
+# steps: choose list for item, find if list has item, change quanity, else put error message
+# output: hash or string
 def change_quantity(list,item,quantity)
   if list.has_key?(item)
     list[item] = quantity
@@ -39,12 +38,13 @@ def change_quantity(list,item,quantity)
   list
 end
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: list
+# steps: iterate through list and print each key, value pair
+# output: string
 def print_list(list)
   list.each {|key,value| puts "You need #{value} #{key}(s)."}
 end
+
 =begin
 p shopping_list = create_list("broom brush pot")
 p add_item(shopping_list,"banana")
