127b43d4110137d51abc2319a6a59f76aef036ba
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-30 14:25:47 -0700

    7.3 - Add function to find longest word in array

--- js/algos.js
+++ js/algos.js
@@ -1,1 +1,13 @@
 //Release 0: Find longest word or phrase from list/array of strings
+
+function longest_word (arr) {
+  str = "";
+  if (arr.length > 0) {
+    str = arr[0];
+  }
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i].length > arr[i-1].length) {
+      str = arr[i]
+    }
+  }
+}
