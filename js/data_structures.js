0845e942d606b97cfa30ff782981b959f67f54f6
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-30 13:17:49 -0700

    Test code for 7.2 data structures

---
+++ js/data_structures.js
@@ -0,0 +1,23 @@
+var colors = ["blue", "red", "green", "yellow"];
+var names = ["Ed", "Bob", "Tony", "Spike"];
+
+
+//Release 1: build an object that takes two arrays and stores each as 'property, value' pair in object
+/*
+var horses = function (arr1, arr2) {
+  list = {};
+  for (var i = 0; i < arr1.length; i++) {
+    list[arr1][i] = "";
+  }
+
+  for (var i = 0; i < arr2.length; i++) {
+    list
+  }
+  return list;
+}
+*/
+
+hash = {};
+
+hash['first'] = "test";
+console.log(hash);
