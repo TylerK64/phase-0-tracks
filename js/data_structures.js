a8bf933f0864b976920f5e7cc3d44cb2bc070417
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-30 13:38:55 -0700

    Rough draft of 7.2 Release 1 - building a horse object

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,23 +1,31 @@
-var colors = ["blue", "red", "green", "yellow"];
 var names = ["Ed", "Bob", "Tony", "Spike"];
+var colors = ["blue", "red", "green", "yellow"];
+
 
 
 //Release 1: build an object that takes two arrays and stores each as 'property, value' pair in object
-/*
+//function assumes arr1 is the array of names & arr2 is the array of colors
+
 var horses = function (arr1, arr2) {
   list = {};
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+
   for (var i = 0; i < arr1.length; i++) {
-    list[arr1][i] = "";
+    var color = arr1[i];
+    list[color] = "";
   }
 
   for (var i = 0; i < arr2.length; i++) {
-    list
+    var name = arr2[i];
+    var color = arr1[i];
+    if (list.hasOwnProperty(color)) {
+      list[color] = name;
+    }
   }
   return list;
 }
-*/
 
-hash = {};
-
-hash['first'] = "test";
-console.log(hash);
+var list = horses (names, colors);
+console.log(list);
