0e3ac5faffc68dfb46ec8285302edaa8167524c2
Tyler Kawabata <tylerkawabata@gmail.com>
2017-05-30 14:31:58 -0700

    7.3 Release 0 - test a couple arrays for longestWord function

--- js/algos.js
+++ js/algos.js
@@ -1,13 +1,21 @@
 //Release 0: Find longest word or phrase from list/array of strings
 
-function longest_word (arr) {
+function longestWord (arr) {
   str = "";
   if (arr.length > 0) {
-    str = arr[0];
+    var str = arr[0];
   }
-  for (i = 1; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     if (arr[i].length > arr[i-1].length) {
       str = arr[i]
     }
+    //else if (arr[i].length == arr[i-1.length]) 
+    //What to do if two lengths are equal?
   }
+  return str;
 }
+
+var arr1 = ["long phrase","longest phrase","longer phrase"];
+var arr2 = ["pear", "apple", "banana", "watermelon", "blueberry"];
+console.log(longestWord(arr1));
+console.log(longestWord(arr2));
