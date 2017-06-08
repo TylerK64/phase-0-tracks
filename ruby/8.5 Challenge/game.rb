0350827d71ff3d53cfdec1a6994c82c6657586b5
Tyler Kawabata <tylerkawabata@gmail.com>
2017-06-08 13:24:02 -0700

    Create files for 8.5 and write some basic pseudocode

---
+++ ruby/8.5 Challenge/game.rb
@@ -0,0 +1,6 @@
+# 8.5 Combining SQL and Ruby
+
+# Release 0: Pseudocode
+# Overall concept: Create a game, "Symbol Crush", with the objective of making combinations of 3 of a kind to rack up points!
+# Possible variables: change size of grid based on user input, change difficulty setting (# of diff symbols) based on user input
+
