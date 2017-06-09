b2399a95b4161bba7df0e1a1f708d551d46737cf
Tyler Kawabata <tylerkawabata@gmail.com>
2017-06-08 18:09:57 -0700

    Create tasks, dates, & locations tables for task_list.rb

--- ruby/8.5 Challenge/task_list.rb
+++ ruby/8.5 Challenge/task_list.rb
@@ -1,16 +1,28 @@
 require 'sqlite3'
 
-def new_task(db, table, task, location, due_date)
-  db.execute()
+def existsCheck(db, table, task_new, location_id_new, due_date_new)
 end
 
-db = SQLite3::Database.new("task_list.db")
-db.results_as_hash = true
+def new_task(db, task_new, location, due_date)
+  db.execute("INSERT INTO tasks (task, location_id, due_date_id) VALUES (?, ?, ?)", [task_new, location, due_date])
+end
+
+def populate_dates(db)
+  days_per_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  days_per_month.length.times do |l|
+    month = l + 1
+    days = days_per_month[l]
+    days.times do |i|
+      date_string = "2017-#{month}-" + (i+1).to_s
+      db.execute("INSERT INTO dates (date) VALUES (?)", date_string)
+    end
+  end
+end
 
 create_dates_table = <<-SQL
   CREATE TABLE IF NOT EXISTS dates (
     id INTEGER PRIMARY KEY,
-    dates TEXT
+    date TEXT
   )
 SQL
 
@@ -32,6 +44,20 @@ create_tasks_table = <<-SQL
   )
 SQL
 
+db = SQLite3::Database.new("task_list.db")
+db.results_as_hash = true
+
 db.execute(create_tasks_table)
+db.execute(create_dates_table)
+db.execute(create_locations_table)
+
+#db.execute("DELETE FROM dates")
+
+# new_task(db, "Walk the duck", 2, 50)
+
+#populate_dates(db)
 
 tasks = db.execute("SELECT * FROM tasks")
+dates = db.execute("SELECT * FROM dates")
+puts tasks
+puts dates
