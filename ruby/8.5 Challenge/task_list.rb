ad3a99125ec848f88df9c0e07e448d4f9e0fe5e3
Tyler Kawabata <tylerkawabata@gmail.com>
2017-06-13 13:43:29 -0700

    Delete tasks method working, able to choose specific line if more than one match

--- ruby/8.5 Challenge/task_list.rb
+++ ruby/8.5 Challenge/task_list.rb
@@ -12,7 +12,7 @@ def db_check(db, task_info) # If exact task, location, & due date already exist 
   SQL
   task_new = db.execute(task_check, [task_info[0], task_info[1], task_info[2]])
 
-  if task_new.empty? # Check + add new location and/or date data if no exact task match; After add task, location_id and due_date_id to tasks
+  if task_new.empty? # Check & add new location and/or date data if no exact task match; After add task, location_id and due_date_id to tasks
     location_new = db.execute("SELECT * FROM locations WHERE location = (?)", task_info[1])
     if location_new.empty?
       db.execute("INSERT INTO locations (location) VALUES (?)", task_info[1])
@@ -30,23 +30,7 @@ def db_check(db, task_info) # If exact task, location, & due date already exist 
   # return true
 end
 
-def populate_dates(db) #Populate dates only for current year. Unnecessary method, used for neater table organization.
-  dates_db = db.execute("SELECT date FROM dates")
-  days_per_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  days_per_month.length.times do |l|
-    month = l + 1
-    days = days_per_month[l]
-    days.times do |i|
-      date_string = "2017-#{month}-" + (i+1).to_s
-      date_new = db.execute("SELECT date FROM dates WHERE date IN (?)", date_string)
-      if !dates_db.include?(date_new.flatten)
-        db.execute("INSERT INTO dates (date) VALUES (?)", date_string)
-      end
-    end
-  end
-end
-
-def task_split(db, task_new)
+def task_split(db, task_new) # Splits input for new task into an array
   task_info = task_new.split(',')
   if task_info.length != 3
     puts "Sorry, not valid input. Try again."
@@ -61,30 +45,66 @@ def update_task(db, task_old, task_update)
   tasks = db.execute("SELECT tasks.task, location_id, due_date_id FROM tasks WHERE tasks.task = (?)", task_old)
   if tasks.empty?
     puts "No such task exists, try entering a different task description."
+    # return false
   elsif tasks.length == 1
     tasks.flatten!
     db.execute("UPDATE tasks SET task = (?) WHERE task = (?) AND location_id = (?) AND due_date_id = (?)", [task_update, task_old, tasks[1], tasks[2]] )
   elsif tasks.length > 1
-    tasks_update = db.execute("SELECT tasks.task, locations.location, dates.date FROM tasks JOIN locations ON tasks.location_id = locations.id JOIN dates ON tasks.due_date_id = dates.id WHERE tasks.task = (?)", task_old)
-    task_num = multiple_tasks(tasks_update)
-    p tasks
+    task_num = multiple_tasks(db, task_old)
+    if task_num < 0 || task_num > tasks.length
+      puts "Not a valid task entry, try a different number."
+      return false
+    end
     task_new = tasks[task_num]
-    p task_new
     db.execute("UPDATE tasks SET task = (?) WHERE task = (?) AND location_id = (?) AND due_date_id = (?)", [task_update, task_old, task_new[1], task_new[2]] )
   end
+end
+
+def delete_task(db, task)
+  tasks = db.execute("SELECT tasks.id, tasks.task FROM tasks WHERE tasks.task = (?)", task)
+  if tasks.empty?
+    puts "No such task exists, try entering a different task description."
+  elsif tasks.length == 1
+    tasks.flatten!
+    db.execute("DELETE FROM tasks WHERE id = (?)", tasks[0])
+  elsif tasks.length > 1
+    task_num = multiple_tasks(db, task)
+    if task_num < 0 || task_num > tasks.length
+      puts "Not a valid task entry, try a different number."
+      return false
+    end
+    task_delete = tasks[task_num]
+    db.execute("DELETE FROM tasks WHERE id = (?)", task_delete[0]) 
+  end  
 end
 
 def search(db, query)
 end
 
-def multiple_tasks(task_arr)
+def multiple_tasks(db, task_query)
+  task_arr = db.execute("SELECT tasks.task, locations.location, dates.date FROM tasks JOIN locations ON tasks.location_id = locations.id JOIN dates ON tasks.due_date_id = dates.id WHERE tasks.task = (?)", task_query)
   puts "There are multiple tasks which fit the entered description, which would you like to choose? Please enter the number of the corresponding output line:"
   task_arr.each do |task|
     puts "Task: #{task[0]} at Location: #{task[1]} with Due Date: #{task[2]}."
   end
   task_num = gets.chomp.to_i
-  task_num -= 1
-  return task_num
+  return task_num - 1
+end
+
+def populate_dates(db) #Populate dates only for current year. Unnecessary method, used for neater table organization.
+  dates_db = db.execute("SELECT date FROM dates")
+  days_per_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  days_per_month.length.times do |l|
+    month = l + 1
+    days = days_per_month[l]
+    days.times do |i|
+      date_string = "2017-#{month}-" + (i+1).to_s
+      date_new = db.execute("SELECT date FROM dates WHERE date IN (?)", date_string)
+      if !dates_db.include?(date_new.flatten)
+        db.execute("INSERT INTO dates (date) VALUES (?)", date_string)
+      end
+    end
+  end
 end
 
 create_dates_table = <<-SQL
@@ -117,15 +137,10 @@ db.execute(create_tasks_table)
 db.execute(create_dates_table)
 db.execute(create_locations_table)
 
-tasks = db.execute("SELECT * FROM tasks")
-dates = db.execute("SELECT * FROM dates")
-#p tasks
-#p dates
-
 
 ## Driver code logic ##
 loop do
-  puts "Please enter 'new task', 'update task', or 'search' to add, edit, or find a task. Or type 'q' to quit."
+  puts "Please enter 'new task', 'update task', 'delete task', or 'search' to add, edit, or find a task. Or type 'q' to quit."
   response = gets.strip
   if response == 'q'
     break
@@ -143,6 +158,10 @@ loop do
     puts "Please enter the updated task information:"
     task_update = gets.strip
     update_task(db, task_old, task_update)
+  elsif response == "delete task"
+    puts "Please enter the name of the task you would like to delete."  
+    task_delete = gets.strip
+    delete_task(db, task_delete)
   elsif response == "search"
     #add code for searching the db later
   else
